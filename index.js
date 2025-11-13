b9d578fc19245b3905f9659bf50f7043c0d4f557
bquang2k6 <phambbquang30@gmail.com>
2025-11-13 23:32:51 +0700

    add

--- index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./server');
-
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log(` Server chạy tại http://localhost:${PORT}`);
-});
--- server.js
+++ server.js
@@ -371,7 +371,7 @@ app.delete('/delete/:id', async (req, res) => {
 });
 
 // ==== Serve static files ====
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
